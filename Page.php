9ba5e6df421e2de8657d0027b55cd94ed953f725
周駿憲 <totem.james@gmail.com>
2020-09-22 10:52:12 +0800

    Update Page.php

--- Page.php
+++ Page.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * 2020-09-22 @ James 
+ * 預設會繼承之前使用的 items 和 now_page
  * 2019-06-03 @ James
  * 後端製作資料分頁 lib
  * 將where過的資料陣列放到 datalist 中就可以擷取特定頁碼資料回傳
@@ -17,6 +19,50 @@
  */
 class Page
 {
+  // 取得items
+  function items(){
+    if(isset($_GET['items'])){
+      if($_GET['items'] == ""){
+          if(isset($_SESSION['items'])){
+            $items = $_SESSION['items'];
+          }else{
+            $items = 10;
+          }
+      }else{
+          $items = $_GET['items'];
+      }
+    }else{
+        if(isset($_SESSION['items'])){
+            $items = $_SESSION['items'];
+        }else{
+            $items = 10;
+        }
+    }
+    return $items;
+  }
+  // 取得items
+  function now_page(){
+    if(isset($_GET['now_page'])){
+      if($_GET['now_page'] == ""){
+          if(isset($_SESSION['now_page'])){
+            $now_page = $_SESSION['now_page'];
+          }else{
+            $now_page = 10;
+          }
+      }else{
+          $now_page = $_GET['now_page'];
+      }
+    }else{
+        if(isset($_SESSION['now_page'])){
+            $now_page = $_SESSION['now_page'];
+        }else{
+            $now_page = 10;
+        }
+    }
+    return $now_page;
+  }
+
+
   // 完整輸出
   function pages_data($arr, $items, $now_page)
   {
@@ -41,6 +87,9 @@ class Page
       $start = $start + 1;
       # code...
     }
+    // 設定緩存 session 狀態
+    $_SESSION['items'] = $items;
+    $_SESSION['now_page'] = $now_page;
     return $res;
   }
   // 取得分頁設定資料
